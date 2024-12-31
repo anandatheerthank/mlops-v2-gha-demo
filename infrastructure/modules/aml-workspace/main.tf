956e06c69bf6b5b72b877bfac323cdb866d1add1
anandatheerthank <31877664+anandatheerthank@users.noreply.github.com>
2024-12-31 12:58:10 +0530

    Update main.tf

--- infrastructure/modules/aml-workspace/main.tf
+++ infrastructure/modules/aml-workspace/main.tf
@@ -18,20 +18,20 @@ resource "azurerm_machine_learning_workspace" "mlw" {
 
 # Compute cluster
 
-resource "azurerm_machine_learning_compute_cluster" "adl_aml_ws_compute_cluster" {
-  name                          = "cpu-cluster"
-  location                      = var.location
-  vm_priority                   = "LowPriority"
-  vm_size                       = "Standard_DS3_v2"
-  machine_learning_workspace_id = azurerm_machine_learning_workspace.mlw.id
-  count                         = var.enable_aml_computecluster ? 1 : 0
+#resource "azurerm_machine_learning_compute_cluster" "adl_aml_ws_compute_cluster" {
+#  name                          = "cpu-cluster"
+ # location                      = var.location
+  #vm_priority                   = "LowPriority"
+  #vm_size                       = "Standard_DS3_v2"
+  #machine_learning_workspace_id = azurerm_machine_learning_workspace.mlw.id
+  #count                         = var.enable_aml_computecluster ? 1 : 0
 
-  scale_settings {
-    min_node_count                       = 0
-    max_node_count                       = 4
-    scale_down_nodes_after_idle_duration = "PT120S" # 120 seconds
-  }
-}
+  #scale_settings {
+   # min_node_count                       = 1
+    #max_node_count                       = 1
+    #scale_down_nodes_after_idle_duration = "PT120S" # 120 seconds
+  #}
+#}
 
 # # Datastore
 
